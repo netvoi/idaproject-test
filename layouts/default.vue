5e14def436548412bf5bc16bc7b949172b1e3825
netvoi <mars.bel@mail.ru>
2021-12-07 02:17:54 +0300

    add: basic styles and main layout

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <Nuxt />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "assets/styles/variables.scss";
+
+.container {
+  margin: 0 auto;
+  max-width: 1440px;
+  padding: 3.2rem;
+}
+</style>
